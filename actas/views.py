ab154a10b8777b3190ea1af8c6a2ae76b907fa9c
Francisco Madrid <fjmadrid@gmail.com>
2016-07-10 21:02:02 -0400

    nueva estructura de vistas

--- actas/views.py
+++ actas/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def lista(request):
+    return render(request, 'lista.html')
+
+
+def subir(request):
+    return render(request, 'subir.html')
